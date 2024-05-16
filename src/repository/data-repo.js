8e3f45707342dc3d9a80919b4107ddf2ffc5810a
AustinNick <46616395+AustinNick@users.noreply.github.com>
2024-05-16 10:05:12 +0700

    implement socketio and cors

--- src/repository/data-repo.js
+++ src/repository/data-repo.js
@@ -1,10 +1,25 @@
+import fire from './../config/database.js'
+var db = fire.firestore()
+import { io } from "./../app/web.js"
+
 const create = async (data) => { 
     // Logic to insert data to database
+    // db.collection(data.collection).add({
+    //     akselerometer: data.akselerometer,
+    //     kelembapan: data.kelembapan,
+    //     tegangan: data.tegangan,
+    //     korosi: data.korosi,
+    // })
+
+    // Send data to client
+    io.emit('data', data);
+
     return data
 }
 
 const findAll = async () => {
     // Logic to get all data from database
+    db.collection()
     return []
 }
 
